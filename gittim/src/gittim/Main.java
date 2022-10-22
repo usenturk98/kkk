b09b386df2f5bb468bbf2a50762ddc090838bbf4
usenturk98 <usenturk98@gmail.com>
2022-10-23 01:01:26 +0300

    update

--- gittim/src/gittim/Main.java
+++ gittim/src/gittim/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Merhaba Github");
 		System.out.println("Merhaba ümit");
+		System.out.println("Merhaba Github");
 	}
 
 }
